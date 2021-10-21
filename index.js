9adfcc60b0bb6b5979bcc0f394e060a4f7a897f2
Garrison <79775636+segarrison@users.noreply.github.com>
2021-10-20 19:47:29 -0500

    Finished index.js code, fixed issue with function name in Employee.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const pageTemplate = require("./src/page-template");
+const myTeam = [];
 
 const managerQ = [
   {
@@ -51,7 +52,7 @@ const engineerQ = [
   },
 ];
 
-const engineerQ = [
+const internQ = [
   {
     type: "input",
     message: "Please enter the intern's name:",
@@ -83,3 +84,66 @@ const options = [
     choices: ["Engineer", "Intern", "Finish"],
   },
 ];
+
+function init() {
+  inquirer.prompt(managerQ).then((response) => {
+    let newManager = new Manager(
+      response.managerName,
+      response.managerID,
+      response.managerEmail,
+      response.managerOfficeNumber
+    );
+    myTeam.push(newManager);
+    askOptions();
+  });
+}
+
+function askOptions() {
+  inquirer.prompt(options).then((response) => {
+    switch (response.optionsList) {
+      case "Engineer":
+        addEngineer();
+        break;
+      case "Intern":
+        addIntern();
+        break;
+      case "Finish":
+        buildPage(myTeam);
+        break;
+    }
+  });
+}
+
+function addEngineer() {
+  inquirer.prompt(engineerQ).then((response) => {
+    let newEngineer = new Engineer(
+      response.engineerName,
+      response.engineerID,
+      response.engineerEmail,
+      response.engineerGitHub
+    );
+    myTeam.push(newEngineer);
+    askOptions();
+  });
+}
+
+function addIntern() {
+  inquirer.prompt(internQ).then((response) => {
+    let newIntern = new Intern(
+      response.internName,
+      response.internID,
+      response.internEmail,
+      response.internSchool
+    );
+    myTeam.push(newIntern);
+    askOptions();
+  });
+}
+
+function buildPage(myTeam) {
+  fs.writeFile("myteam.html", pageTemplate(myTeam), (err) =>
+    err ? console.log(err) : console.log("Succesfully created myteam.html!")
+  );
+}
+
+init();
